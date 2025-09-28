8910e13e442861c0a28a4cd89ff2825303671289
1183668245 <169026345+1183668245@users.noreply.github.com>
2025-09-28 15:49:23 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,16 @@
+ 
+const API_CONFIG = {
+    BASE_URL: process.env.NODE_ENV === 'development' 
+        ? 'https://localhost:3001/api'   
+        : 'https://api.pmelody.top/api',
+    ENDPOINTS: {
+        AUTH: '/auth',
+        USERS: '/users', 
+        LYRICS: '/lyrics',
+        SONGS: '/songs',
+        NFT: '/nft'
+    }
+};
+
+ 
+window.API_CONFIG = API_CONFIG;
